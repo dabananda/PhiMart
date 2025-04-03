76c0a1f0c52caf8b582211ed9f3e19753234ffd4
dabananda <imdmitra@gmail.com>
2025-04-04 00:59:30 +0600

    Implemented sorting

--- product/views.py
+++ product/views.py
@@ -4,15 +4,16 @@ from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import ProductFilter
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['name', 'description']
+    ordering_fields = ['price', 'updated_at']
 
 
 class CategoryViewSet(ModelViewSet):
