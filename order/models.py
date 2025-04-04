aba3d311e4dfc7bc3ee0cd38307b8af0ee4e94ca
dabananda <imdmitra@gmail.com>
2025-04-04 09:36:41 +0600

    Updated Cart & CartItem models

--- order/models.py
+++ order/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from product.models import Product
 from users.models import User
+from uuid import uuid4
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Cart of {self.user.username}"
+        return f"Cart of {self.user.first_name}"
 
 
 class CartItem(models.Model):
@@ -17,6 +19,9 @@ class CartItem(models.Model):
         Cart, on_delete=models.CASCADE, related_name="cart_items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
     def __str__(self):
         return f"{self.product.name} x {self.quantity}"
