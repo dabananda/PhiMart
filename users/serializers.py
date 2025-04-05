90522c3ca2622cf6a535da2e0270f71f7005dc15
dabananda <imdmitra@gmail.com>
2025-04-05 18:24:24 +0600

    Login user

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,13 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'first_name', 'last_name',
                   'email', 'phone_number', 'password', 'address']
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'phone_number', 'address']
